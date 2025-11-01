170e67e35205ae530b46f38906c120e05e002fcc
Edward NoaLand <edwardnoaland@gmail.com>
2025-11-01 19:11:01 +0000

    feat: add syntax design

--- src/interpreter/repl/repl.cpp
+++ src/interpreter/repl/repl.cpp
@@ -1,7 +1,20 @@
-#include <iostream>
-
 int main() {
-	std::cout << "hello world!" << std::endl;
+	/**
+	 * V1
+	 * const source src = my_scheme::frontend::io::input_reader{std::cin}.process();
+	 * const auto tokens = my_scheme::frontend::lexer.tokenize(src);
+	 * const auto ast = my_scheme::frontend::parser.parse(tokens);
+	 * my_scheme::repl.execute(ast);
+	 */
+
+	/**
+	 * V2: use pipeline operator refactor
+	 * using namespace my_scheme::frontend;
+	 * io::input_reader{std::cin}.process()
+	 *     | lexer::tokenize
+	 *     | parser::parse
+	 *     | repl::execute;
+	 */
 
 	return 0;
 }
